d3a78009db689ad125f0c3fe45832088a5cde677
Misgan Moges <misgewsatlmay@gmail.com>
2024-11-08 10:07:52 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,39 +1,10 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"github.com/SolomonAHailu/one-card-system/initializers"
-// 	"github.com/SolomonAHailu/one-card-system/server"
-// )
-
-// func init() {
-// 	initializers.LoadEnvVariables()
-// 	initializers.ConnectToDB(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-// 	// migrations.Migrate(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-// }
-
-// func main() {
-// 	if initializers.DB == nil {
-// 		log.Fatal("Database connection is not initialized")
-// 	}
-// 	s := server.NewServer()
-// 	s.Initialize()
-// 	serverPort := fmt.Sprintf(":%s", os.Getenv("API_PORT"))
-// 	s.Run(serverPort)
-// }
-
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
-	"time"
-
-	"github.com/SolomonAHailu/one-card-system/backend/controllers/registrarcontrollers"
+        "time"
 	"github.com/SolomonAHailu/one-card-system/initializers"
 	"github.com/SolomonAHailu/one-card-system/server"
 )
@@ -48,16 +19,14 @@ func main() {
 	if initializers.DB == nil {
 		log.Fatal("Database connection is not initialized")
 	}
-
-	// Start periodic sync as a Goroutine
-	go startPeriodicSync()
-
-	// Initialize and run the server
+        go startPeriodicSync()
 	s := server.NewServer()
 	s.Initialize()
 	serverPort := fmt.Sprintf(":%s", os.Getenv("API_PORT"))
 	s.Run(serverPort)
 }
+
+
 
 // startPeriodicSync sets up a periodic sync with a specific interval
 func startPeriodicSync() {
