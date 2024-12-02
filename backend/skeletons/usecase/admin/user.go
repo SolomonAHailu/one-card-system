96d432412c13edae987f56e29e00054f8fda0523
Misgan Moges <misgewsatlmay@gmail.com>
2024-12-02 17:27:01 +0300

    user management test compleleted

--- backend/skeletons/usecase/admin/user.go
+++ backend/skeletons/usecase/admin/user.go
@@ -1,1 +1,38 @@
 package adminUsecase
+
+import (
+	"github.com/SolomonAHailu/one-card-system/models/adminmodels"
+	adminRepo "github.com/SolomonAHailu/one-card-system/skeletons/repo/admin"
+	// "github.com/gin-gonic/gin"
+	// "gorm.io/gorm"
+)
+
+type userUsecase struct {
+	userRespository adminRepo.UserRepository
+}
+
+
+// NewUserUsecase will create new an userUsecase object representation of userUsecase.UserUsecase interface
+func NewUserUsecase(userRespository adminRepo.UserRepository) adminRepo.UserUsecase {
+	return &userUsecase{
+	userRespository: userRespository,
+	}
+}
+
+
+// CreatedUser implements usermanagement.UserUsecase.
+func (d *userUsecase) CreateUser(createUser *adminRepo.CreateUserRequest) (adminmodels.Users, error) {
+	return d.userRespository.CreateUser(createUser)
+}
+
+func (d *userUsecase) GetUsersByRoleId(createUser *adminRepo.CreateUserRequest) (adminmodels.Users, error) {
+	return d.userRespository.GetUsersByRoleId(createUser)
+}
+
+func (d *userUsecase) UpdateUserById(createUser *adminRepo.CreateUserRequest) (adminmodels.Users, error) {
+	return d.userRespository.UpdateUserById(createUser)
+}
+
+func (d *userUsecase) DeleteUserById(createUser *adminRepo.CreateUserRequest) (adminmodels.Users, error) {
+	return d.userRespository.DeleteUserById(createUser)
+}
--- backend/test/admins/user_test.go
+++ backend/test/admins/user_test.go
@@ -1,1 +1,216 @@
 package usecase_test
+
+import (
+	"errors"
+	"testing"
+	"github.com/SolomonAHailu/one-card-system/skeletons/mocks"
+	"github.com/SolomonAHailu/one-card-system/skeletons/repo/admin"
+	"github.com/SolomonAHailu/one-card-system/skeletons/usecase/admin"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+
+	// "github.com/stretchr/testify/mock"
+	"github.com/SolomonAHailu/one-card-system/models/adminmodels"
+)
+
+func TestCreateUser(t *testing.T) {
+	mockUser := mocks.NewUserRepository(t)
+	cc := &gin.Context{}
+	dbb := &gorm.DB{}
+
+	mockCreateUserRequest := &adminRepo.CreateUserRequest{
+		Ctx : cc,
+		Gm : dbb,
+	}
+
+	mockUserTest := adminmodels.Users{
+		FirstName: "123",
+		FatherName: "123",
+		GrandFatherName: "123",
+		Email: "123",
+		Password: "123",
+		UnhashedPassword: "123",
+		RoleId: 1,
+		Role: adminmodels.Roles{
+			RoleName: "123",
+			Description: "123",
+		},
+	}
+
+	t.Run("success", func(t *testing.T){
+
+		mockUser.On("CreateUser",  mockCreateUserRequest).Return(mockUserTest, nil).Once()
+		uc := adminUsecase .NewUserUsecase(mockUser)
+		user, err := uc.CreateUser(mockCreateUserRequest)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockUserTest, user)
+		mockUser.AssertExpectations(t)
+	})
+
+
+	t.Run("fail", func(t *testing.T){
+
+		mockUser.On("CreateUser",  mockCreateUserRequest).Return(adminmodels.Users{}, errors.New("unexpected")).Once()
+		uc := adminUsecase .NewUserUsecase(mockUser)
+		user, err := uc.CreateUser(mockCreateUserRequest)
+
+		assert.Error(t, err)
+		assert.Equal(t, user, adminmodels.Users{})
+		mockUser.AssertExpectations(t)
+	})
+}
+
+func TestGetUsersByRoleId(t *testing.T) {
+	mockUser := mocks.NewUserRepository(t)
+	cc := &gin.Context{}
+	dbb := &gorm.DB{}
+
+	mockCreateUserRequest := &adminRepo.CreateUserRequest{
+		Ctx : cc,
+		Gm : dbb,
+	}
+
+	mockUserTest := adminmodels.Users{
+		FirstName: "123",
+		FatherName: "123",
+		GrandFatherName: "123",
+		Email: "123",
+		Password: "123",
+		UnhashedPassword: "123",
+		RoleId: 1,
+		Role: adminmodels.Roles{
+			RoleName: "123",
+			Description: "123",
+
+		},
+
+	}
+
+	t.Run("success", func(t *testing.T){
+
+		mockUser.On("GetUsersByRoleId",  mockCreateUserRequest).Return(mockUserTest, nil).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.GetUsersByRoleId(mockCreateUserRequest)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockUserTest, user)
+		mockUser.AssertExpectations(t)
+	})
+
+
+	t.Run("fail", func(t *testing.T){
+
+		mockUser.On("GetUsersByRoleId",  mockCreateUserRequest).Return(adminmodels.Users{}, errors.New("unexpected")).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.GetUsersByRoleId(mockCreateUserRequest)
+
+		assert.Error(t, err)
+		assert.Equal(t, user, adminmodels.Users{})
+		mockUser.AssertExpectations(t)
+	})
+}
+
+
+
+
+func TestUpdateUserById(t *testing.T) {
+	mockUser := mocks.NewUserRepository(t)
+	cc := &gin.Context{}
+	dbb := &gorm.DB{}
+
+	mockCreateUserRequest := &adminRepo.CreateUserRequest{
+		Ctx : cc,
+		Gm : dbb,
+	}
+
+	mockUserTest := adminmodels.Users{
+		FirstName: "123",
+		FatherName: "123",
+		GrandFatherName: "123",
+		Email: "123",
+		Password: "123",
+		UnhashedPassword: "123",
+		RoleId: 1,
+		Role: adminmodels.Roles{
+			RoleName: "123",
+			Description: "123",
+
+		},
+
+	}
+
+	t.Run("success", func(t *testing.T){
+
+		mockUser.On("UpdateUserById",  mockCreateUserRequest).Return(mockUserTest, nil).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.UpdateUserById(mockCreateUserRequest)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockUserTest, user)
+		mockUser.AssertExpectations(t)
+	})
+
+
+	t.Run("fail", func(t *testing.T){
+
+		mockUser.On("UpdateUserById",  mockCreateUserRequest).Return(adminmodels.Users{}, errors.New("unexpected")).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.UpdateUserById(mockCreateUserRequest)
+
+		assert.Error(t, err)
+		assert.Equal(t, user, adminmodels.Users{})
+		mockUser.AssertExpectations(t)
+	})
+}
+
+
+func TestDeleteUserById(t *testing.T) {
+	mockUser := mocks.NewUserRepository(t)
+	cc := &gin.Context{}
+	dbb := &gorm.DB{}
+
+	mockCreateUserRequest := &adminRepo.CreateUserRequest{
+		Ctx : cc,
+		Gm : dbb,
+	}
+
+	mockUserTest := adminmodels.Users{
+		FirstName: "123",
+		FatherName: "123",
+		GrandFatherName: "123",
+		Email: "123",
+		Password: "123",
+		UnhashedPassword: "123",
+		RoleId: 1,
+		Role: adminmodels.Roles{
+			RoleName: "123",
+			Description: "123",
+
+		},
+	}
+
+	t.Run("success", func(t *testing.T){
+
+		mockUser.On("DeleteUserById",  mockCreateUserRequest).Return(mockUserTest, nil).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.DeleteUserById(mockCreateUserRequest)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockUserTest, user)
+		mockUser.AssertExpectations(t)
+	})
+
+
+	t.Run("fail", func(t *testing.T){
+
+		mockUser.On("DeleteUserById",  mockCreateUserRequest).Return(adminmodels.Users{}, errors.New("unexpected")).Once()
+		uc := adminUsecase.NewUserUsecase(mockUser)
+		user, err := uc.DeleteUserById(mockCreateUserRequest)
+
+		assert.Error(t, err)
+		assert.Equal(t, user, adminmodels.Users{})
+		mockUser.AssertExpectations(t)
+	})
+}
